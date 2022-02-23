1431c973da70e7a5be58ae47da0ede1924eea609
Justin Jones <jus.jones@yahoo.com>
2022-02-23 13:05:39 -0500

    commit tree f026f46170c0983419e2be0ad6a566f5095813fd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+
+
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+/*
+const companyCeo = "Susan Smith"; */
+
+let companyCeo = "Susan Smith";
